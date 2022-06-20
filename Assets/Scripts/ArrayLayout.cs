c8bea86459ff0cc69ba426844861d868fa5e0202
Sebastian Knabe <knabe@ad-artists.de>
2022-06-20 10:13:20 +0200

    setup repo for prototyp

---
+++ Assets/Scripts/ArrayLayout.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class ArrayLayout  {
+
+	public int width;
+	public int height;
+
+	[System.Serializable]
+	public struct rowData{
+		public bool[] row;
+	}
+
+    public Grid grid;
+    public rowData[] rows = new rowData[14]; //Grid of 9x14
+}
---
+++ Assets/Scripts/Match3.cs
@@ -0,0 +1,312 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match3 : MonoBehaviour
+{
+    public ArrayLayout boardLayout;
+
+    [Header("UI Elements")]
+    public Sprite[] pieces;
+    public RectTransform gameBoard;
+    public RectTransform killedBoard;
+
+    [Header("Prefabs")]
+    public GameObject nodePiece;
+    public GameObject killedPiece;
+
+    [Header("Game")]
+    public int maxScore;
+    public int score = 0;
+
+    int width = 9;
+    int height = 14;
+    int[] fills;
+    Node[,] board;
+
+    List<NodePiece> update;
+    List<FlippedPieces> flipped;
+    List<NodePiece> dead;
+    List<KilledPiece> killed;
+
+    System.Random random;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void Update()
+    {
+        
+        List<NodePiece> finishedUpdating = new List<NodePiece>();
+        for(int i = 0; i < update.Count; i++)
+        {
+            NodePiece piece = update[i];
+            if (!piece.UpdatePiece()) finishedUpdating.Add(piece);
+        }
+    }
+
+    public void ApplyGravityToBoard()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = (height - 1); y >= 0; y--) //Start at the bottom and grab the next
+            {
+                Point p = new Point(x, y);
+                Node node = getNodeAtPoint(p);
+                int val = getValueAtPoint(p);
+                if (val != 0) continue; //If not a hole, move to the next
+                for (int ny = (y - 1); ny >= -1; ny--)
+                {
+                    Point next = new Point(x, ny);
+                    int nextVal = getValueAtPoint(next);
+                    if (nextVal == 0)
+                        continue;
+                    if (nextVal != -1)
+                    {
+                        Node gotten = getNodeAtPoint(next);
+                        NodePiece piece = gotten.getPiece();
+
+                        //Set the hole
+                        node.SetPiece(piece);
+                        update.Add(piece);
+
+                        //Make a new hole
+                        gotten.SetPiece(null);
+                    }
+                    else//Use dead ones or create new pieces to fill holes (hit a -1) only if we choose to
+                    {
+                        int newVal = fillPiece();
+                        NodePiece piece;
+                        Point fallPnt = new Point(x, (-1 - fills[x]));
+                        if(dead.Count > 0)
+                        {
+                            NodePiece revived = dead[0];
+                            revived.gameObject.SetActive(true);
+                            piece = revived;
+
+                            dead.RemoveAt(0);
+                        }
+                        else
+                        {
+                            GameObject obj = Instantiate(nodePiece, gameBoard);
+                            NodePiece n = obj.GetComponent<NodePiece>();
+                            piece = n;
+                        }
+
+                        piece.Initialize(newVal, p, pieces[newVal - 1]);
+                        piece.rect.anchoredPosition = getPositionFromPoint(fallPnt);
+
+                        Node hole = getNodeAtPoint(p);
+                        hole.SetPiece(piece);
+                        ResetPiece(piece);
+                        fills[x]++;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    void StartGame()
+    {
+        fills = new int[width];
+        string seed = getRandomSeed();
+        random = new System.Random(seed.GetHashCode());
+        update = new List<NodePiece>();
+        flipped = new List<FlippedPieces>();
+        dead = new List<NodePiece>();
+        killed = new List<KilledPiece>();
+
+        InitializeBoard();
+        //VerifyBoard();
+        InstantiateBoard();
+    }
+
+    void InitializeBoard()
+    {
+        board = new Node[width, height];
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                board[x, y] = new Node((boardLayout.rows[y].row[x]) ? - 1 : fillPiece(), new Point(x, y));
+            }
+        }
+    }
+
+    void InstantiateBoard()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Node node = getNodeAtPoint(new Point(x, y));
+
+                int val = node.value;
+                if (val <= 0) continue;
+                GameObject p = Instantiate(nodePiece, gameBoard);
+                NodePiece piece = p.GetComponent<NodePiece>();
+                RectTransform rect = p.GetComponent<RectTransform>();
+                rect.anchoredPosition = new Vector2(32 + (64 * x), -32 - (64 * y));
+                piece.Initialize(val, new Point(x, y), pieces[val - 1]);
+                node.SetPiece(piece);
+            }
+        }
+    }
+     
+    public void ResetPiece(NodePiece piece)
+    {
+        piece.ResetPosition();
+        update.Add(piece);
+    }
+
+    public void FlipPieces(Point one, Point two, bool main)
+    {
+        if (getValueAtPoint(one) < 0) return;
+
+        Node nodeOne = getNodeAtPoint(one);
+        NodePiece pieceOne = nodeOne.getPiece();
+        if (getValueAtPoint(two) > 0)
+        {
+            Node nodeTwo = getNodeAtPoint(two);
+            NodePiece pieceTwo = nodeTwo.getPiece();
+            nodeOne.SetPiece(pieceTwo);
+            nodeTwo.SetPiece(pieceOne);
+
+            if(main)
+                flipped.Add(new FlippedPieces(pieceOne, pieceTwo));
+
+            update.Add(pieceOne);
+            update.Add(pieceTwo);
+        }
+        else
+            ResetPiece(pieceOne);
+    }
+
+    public void KillPiece(Point p, bool getPoints)
+    {
+        List<KilledPiece> available = new List<KilledPiece>();
+        for (int i = 0; i < killed.Count; i++)
+            if (!killed[i].falling) available.Add(killed[i]);
+
+        KilledPiece set = null;
+        if (available.Count > 0)
+        {
+            set = available[0];
+        }
+        
+        else
+        {
+            GameObject kill = GameObject.Instantiate(killedPiece, killedBoard);
+            KilledPiece kPiece = kill.GetComponent<KilledPiece>();
+            set = kPiece;
+            killed.Add(kPiece);
+        }
+
+        int val = getValueAtPoint(p) - 1;
+        if (set != null && val >= 0 && val < pieces.Length)
+            set.Initialize(pieces[val], getPositionFromPoint(p));
+
+        Node node = getNodeAtPoint(p);
+        NodePiece nodePiece = node.getPiece();
+        if (nodePiece != null)
+        {
+            nodePiece.gameObject.SetActive(false);
+            dead.Add(nodePiece);
+        }
+        node.SetPiece(null);
+
+        ApplyGravityToBoard();
+
+        if(getPoints)
+            score++;
+    }
+
+    int fillPiece()
+    {
+        int val = 1;
+        val = (random.Next(0, 100) / (100 / pieces.Length)) + 1;
+        return val;
+    }
+
+    int getValueAtPoint(Point p)
+    {
+        if (p.x < 0 || p.x >= width || p.y < 0 || p.y >= height) return -1;
+        return board[p.x, p.y].value;
+    }
+
+    void setValueAtPoint(Point p, int v)
+    {
+        board[p.x, p.y].value = v;
+    }
+
+    Node getNodeAtPoint(Point p)
+    {
+        return board[p.x, p.y];
+    }
+
+    string getRandomSeed()
+    {
+        string seed = "";
+        string acceptableChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdeghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
+        for (int i = 0; i < 20; i++)
+            seed += acceptableChars[Random.Range(0, acceptableChars.Length)];
+        return seed;
+    }
+
+    public Vector2 getPositionFromPoint(Point p)
+    {
+        return new Vector2(32 + (64 * p.x), -32 - (64 * p.y));
+    }
+}
+
+[System.Serializable]
+public class Node
+{
+    public int value; //0 = blank, 1 = cube, 2 = sphere, 3 = cylinder, 4 = pryamid, 5 = diamond, -1 = hole
+    public Point index;
+    NodePiece piece;
+
+    public Node(int v, Point i)
+    {
+        value = v;
+        index = i;
+    }
+
+    public void SetPiece(NodePiece p)
+    {
+        piece = p;
+        value = (piece == null) ? 0 : piece.value;
+        if (piece == null) return;
+        piece.SetIndex(index);
+    }
+
+    public NodePiece getPiece()
+    {
+        return piece;
+    }
+}
+
+[System.Serializable]
+public class FlippedPieces
+{
+    public NodePiece one;
+    public NodePiece two;
+
+    public FlippedPieces(NodePiece o, NodePiece t)
+    {
+        one = o; two = t;
+    }
+
+    public NodePiece getOtherPiece(NodePiece p)
+    {
+        if (p == one)
+            return two;
+        else if (p == two)
+            return one;
+        else
+            return null;
+    }
+}
